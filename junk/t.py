2ac185a90286f66dc2ef4044a00a9f71c01dca86
ELOUTMADI <eloutmadiabderrahim@gmail.com>
2024-04-05 02:55:07 +0000

    Remove ignored files

--- junk/t.py
+++
@@ -1,8 +0,0 @@
-for i in range(5):
-    try:
-        if i == 2:
-            raise Exception("Simulated error")
-    except Exception as e:
-        print(f"Handling exception for i={i}: {e}")
-        continue  # Skips the rest of the loop body for this iteration
-    print(f"Processing i={i}")
