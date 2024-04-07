6968f47eab3bb0849ff06589224b80f2b7ca548c
ELOUTMADI <eloutmadiabderrahim@gmail.com>
2024-04-07 03:13:33 +0000

    Defining all columns

--- data_processing/reading_data_from_hadoop.py
+++ data_processing/reading_data_from_hadoop.py
@@ -1,9 +1,7 @@
-
-from pyspark.sql import SparkSession
 import json
-from pyspark.sql.types import StructType, StructField, StringType, ArrayType
-from pyspark.sql.functions import col, from_json
-from pyspark.sql import Row
+from pyspark.sql import SparkSession
+
+
 def read_json_from_hadoop_with_spark(hdfs_path):
     """
     Reads a JSON file from Hadoop HDFS using Spark, parses its content, and shows it as a DataFrame.
@@ -18,7 +16,7 @@ def read_json_from_hadoop_with_spark(hdfs_path):
     # Initialize Spark configuration and context
     spark = SparkSession.builder.appName("Read JSON from Hadoop").getOrCreate()
 
-    try :
+    try:
         # Read the JSON file content as RDD
         json_rdd = spark.sparkContext.textFile(hdfs_path)
 
@@ -31,7 +29,7 @@ def read_json_from_hadoop_with_spark(hdfs_path):
         json_rdd = spark.sparkContext.parallelize(json.loads(second_parsing))
 
         # Create a DataFrame from the RDD by flattening the JSON structure into key-value pairs
-        parsed_rdd = json_rdd.flatMap(lambda job: [(key, (value)) for key, value in job.items()])
+        #parsed_rdd = json_rdd.flatMap(lambda job: [(key, (value)) for key, value in job.items()])
 
         df = spark.createDataFrame(json_rdd)
 
@@ -42,16 +40,4 @@ def read_json_from_hadoop_with_spark(hdfs_path):
         print(f"An error occurred in reading JSon FIle from HDFS using spark: {e}")
         return {}
 
-if __name__ == "__main__":
-    hdfs_path = 'hdfs://localhost:9000/data_lake/raw/jobs/health_care_research_pharmacy/2024/03/31/health_care_research_pharmacy_20240331T032123.json'
-    df = read_json_from_hadoop_with_spark(hdfs_path)
-    schema = StructType([
-        StructField("job_title", StringType(), True),
-        StructField("company_name", StringType(), True),
-        StructField("location", StringType(), True),
-        StructField("promoted_status", StringType(), True),
-        StructField("easy_apply_status", StringType(), True)
-    ])
-    df = df.withColumn("parsed_details", from_json("details", schema))
-    df = df.withColumn("job_title", col("parsed_details.job_title"))
-    df.show(truncate=False)
+
