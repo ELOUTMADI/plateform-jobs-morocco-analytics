aee5e56acac4c7a6e67f2360931d2f00809be783
ELOUTMADI <eloutmadiabderrahim@gmail.com>
2024-05-23 17:53:37 +0100

    username and email vaidation form

---
+++ django_part/dashboardpage/authentification/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+
+from . import views
+from .views import UsernameValidationView, EmailValidationView
+
+urlpatterns = [
+
+    path('register', views.register, name='register'),
+    path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name='validate-username'),
+    path('validate-email', csrf_exempt(EmailValidationView.as_view()) , name='validate-email')
+]
+
--- django_part/dashboardpage/dashboardpage/urls.py
+++ django_part/dashboardpage/dashboardpage/urls.py
@@ -19,5 +19,6 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('first_page.urls'))
+    path('', include('first_page.urls')),
+    path('authentification/',include('authentification.urls'))
 ]
